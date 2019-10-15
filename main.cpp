4bbd09c0102a734873bc0b4f89288cbf4655f417
Sadegh hosseiney <sadeghmessi2015@gmail.com>
2019-10-15 18:31:32 -0400

    create first command

--- main.cpp
+++ main.cpp
@@ -16,23 +16,38 @@ int main()
 void start()
 {
     string command;
-    vector<string> wow;
     while(true)
     {
+        vector<string> placeCommand;
         cout << "database > ";
+        string temp = "";
         getline(cin, command);
-        wow.push_back(command);
-        for (int i = 0; command[i]!= '\0'; i ++)
+             for (int i = 0; command[i] != '\0'; i ++)
         {
             if (command[i] >= 'A' && command[i] <= 'Z')
             {
                 command[i] = command[i] + 32;
             }
         }
-        if (command == "basu add class")
+        command += ' ';
+        for (char c : command)
         {
+            if (c != ' ')
+            {
+                temp += c;
+            }
+            else
+            {
+                placeCommand.push_back(temp);
+
+                temp = "";
+            }
+        }
+        if (placeCommand.size() == 3 && placeCommand[0] == "basu" && placeCommand[1] == "add" && placeCommand[2] == "class")
+        {
+            cout << "it's right" << endl;
+            placeCommand.clear();
             break;
         }
-
     }
 }
